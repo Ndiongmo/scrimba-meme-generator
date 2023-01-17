bc74c83736d47cb878ca5a0d0f92d6ae9b41474d
Ndiongmo <mauricendiongmo@gmail.com>
2023-01-17 16:00:54 +0100

    Dynamic Styles

--- src/components/Main/index.jsx
+++ src/components/Main/index.jsx
@@ -4,12 +4,20 @@ import Header from "../Header";
 import Meme from "../Meme";
 
 import { boxesData } from "../../datas/boxeData";
+import React from "react";
 
-function App() {
-    const boxeElements = boxesData.map((boxe) => (
-        <div className="box" key={boxe.id}>
-            {boxe.id}
-        </div>
+function App(props) {
+    // Challenge: use a ternary to determine the backgroundColor.
+    // If darkMode is true, set it to "#222222"
+    // If darkMode is false, set it to "#cccccc"
+
+    const [boxes, setBoxe] = React.useState(boxesData);
+
+    const styles = {
+        backgroundColor: props.darkMode ? "#222222" : "#cccccc",
+    };
+    const boxeElements = boxes.map((boxe) => (
+        <div style={styles} className="box" key={boxe.id}></div>
     ));
     return (
         <div className="App">
