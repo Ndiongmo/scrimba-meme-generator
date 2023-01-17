d0d2c59bccc8fd61c6a9a2497e15f1cf9bb844ea
Ndiongmo <mauricendiongmo@gmail.com>
2023-01-17 15:24:52 +0100

    Boxe challenge: part1

---
+++ src/components/Boxe/index.jsx
@@ -0,0 +1,15 @@
+export default function Boxe() {
+    /**
+     * Challenge part 1:
+     * 1. Initialize state with the default value of the
+     *    array pulled in from boxes.js
+     * 2. Map over that state array and display each one
+     *    as an empty square (black border, transparent bg color)
+     *    (Don't worry about using the "on" property yet)
+     */
+    return (
+        <div>
+            <h1>Boxe Challenge</h1>
+        </div>
+    );
+}
--- src/components/Main/index.jsx
+++ src/components/Main/index.jsx
@@ -3,11 +3,20 @@ import "../../styles/App.css";
 import Header from "../Header";
 import Meme from "../Meme";
 
+import { boxesData } from "../../datas/boxeData";
+
 function App() {
+    const boxeElements = boxesData.map((boxe) => (
+        <div className="box" key={boxe.id}>
+            {boxe.id}
+        </div>
+    ));
     return (
         <div className="App">
             <Header />
             <Meme />
+            <hr></hr>
+            {boxeElements}
         </div>
     );
 }
